574cce3b02a1aa74ed70f0b317c6516369ab13c4
BearK <harry.hsu0515@gmail.com>
2023-04-16 00:35:24 +0800

    測試環境變數

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APP_API_KEY,
+  apiKey: "AIzaSyAbkh825tjo6uJ1_zfO6tD_-2rUP0OdZu0",
   authDomain: "my-profile-web-92291.firebaseapp.com",
   projectId: "my-profile-web-92291",
   storageBucket: "my-profile-web-92291.appspot.com",
-  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_APP_ID,
+  messagingSenderId: "54386889065",
+  appId: "1:54386889065:web:bb44eaf351e107646eadf8",
 };
 
 const app = initializeApp(firebaseConfig);
